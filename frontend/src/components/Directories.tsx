35e67ed39f80cbd3b572fadac02356ed2b14cad0
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2025-03-30 01:42:38 -0300

    feat: add frontend signalr interaction

--- frontend/src/components/Directories.tsx
+++ frontend/src/components/Directories.tsx
@@ -1,14 +1,23 @@
 import {useEffect, useState} from 'react'
 import {IDirectory} from "@/types/directory";
+import {buildFileSystemHub} from "@/services/fileSystemHubService.ts";
 import {getDirectories} from "@/services/directoriesService.ts";
 import {localSetItem} from "@/services/storeService.ts";
 import {FaFolder, FaFolderOpen} from "react-icons/fa6";
 import {Link} from "react-router";
 import Loading from "@/components/Loading.tsx";
+import {HubConnectionState} from "@microsoft/signalr";
 
 export default function Directories() {
     const [isLoading, setIsLoading] = useState(false);
     const [directories, setDirectories] = useState<IDirectory[]>([]);
+
+    const fileSystemHub = buildFileSystemHub();
+    fileSystemHub.on("Changed", async () => {
+        const directories = await getDirectories();
+        localSetItem('directories', directories);
+        setDirectories(directories);
+    });
 
     function countParent(directory: IDirectory, count = 0) {
         const parentDirectory = directories.find(x => x.id === directory.parentId);
@@ -26,8 +35,13 @@ export default function Directories() {
     useEffect(() => {
         setIsLoading(true);
 
-        Promise.all([getDirectories()])
-            .then(([directories]) => {
+        const startFileSystemHub = fileSystemHub.state === HubConnectionState.Disconnected
+            ? fileSystemHub.start()
+            : Promise.resolve();
+
+        Promise.all([startFileSystemHub, getDirectories()])
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            .then(([_, directories]) => {
                 localSetItem('directories', directories);
                 setDirectories(directories);
             })
@@ -37,6 +51,12 @@ export default function Directories() {
             .finally(() => {
                 setIsLoading(false);
             });
+
+        return () => {
+            if (fileSystemHub.state !== HubConnectionState.Connecting && fileSystemHub.state !== HubConnectionState.Reconnecting) {
+                void fileSystemHub.stop();
+            }
+        };
     }, []);
 
     if (isLoading) {
