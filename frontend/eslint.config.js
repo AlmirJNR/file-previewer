e6463dfe33a705a1f259703f9ffe36d476eff829
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2026-04-11 00:54:19 -0300

    deps: update major dependencies

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,5 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import {default as reactX} from "eslint-plugin-react-x";
@@ -24,13 +23,11 @@ export default tseslint.config(
             },
         },
         plugins: {
-            'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
             'react-x': reactX,
             'react-dom': reactDom,
         },
         rules: {
-            ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': [
                 'warn',
                 {allowConstantExport: true},
